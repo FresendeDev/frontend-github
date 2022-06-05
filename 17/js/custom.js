5577ddfeafca6b99212765a42948e6380a021f34
Fabio Resende <fabio.resende@gmail.com>
2022-06-05 19:51:56 -0300

    inicio 17

---
+++ 17/js/custom.js
@@ -0,0 +1,12 @@
+jQuery(function ($) {});
+// $(document).ready(function () {});
+
+$(".owl-carousel").owlCarousel({
+  //   items: 3,
+  lazyLoad: true,
+  //   loop: true,
+  //   margin: 10,
+});
+
+$(".featured-item a").addClass("btn btn-primary");
+$(".featured-item").addClass("p-2");
