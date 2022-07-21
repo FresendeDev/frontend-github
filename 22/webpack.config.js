395ae101dbe9949b955b1d4275bc82346bfa40dc
Fabio Resende <fabio.resende@gmail.com>
2022-07-21 10:50:16 -0300

    conclusao 22

--- 22/webpack.config.js
+++ 22/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const htmlWebpack = require("html-webpack-plugin");
+const miniCssExtract = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,10 +8,45 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|c|sc)ss$/,
+        use: [miniCssExtract.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_mudules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(jpeg|jpg|png|svg|gif)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
+      },
+    ],
+  },
+
   plugins: [
     new htmlWebpack({
       filename: "index.html",
       template: "./src/index.html",
+    }),
+    new miniCssExtract({
+      filename: "style.css",
     }),
   ],
 };
